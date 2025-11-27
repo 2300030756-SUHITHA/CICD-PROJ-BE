d4a81fd353ed3c33358a17516ba829b180e0c950
2300030756-SUHITHA <162713778+2300030756-SUHITHA@users.noreply.github.com>
2025-11-28 01:30:22 +0530

    Update AuthController.java

--- src/main/java/com/klu/AuthController.java
+++ src/main/java/com/klu/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 @CrossOrigin(origins = "http://localhost:5173")
-@RequestMapping("/back")
+@RequestMapping("/back/")
 
 
 public class AuthController {
